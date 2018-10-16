e68a3eb119328c8de616df1088fa833010d8e58a
Daniel Whiteman <daniel.whiteman1@gmail.com>
2018-10-16 12:26:13 -0500

    Final update plus commenting

--- js/script.js
+++ js/script.js
@@ -1,45 +1,47 @@
 // FSJS - Random Quote Generator
+/*Resources Used In This Project:
+  Treehouse unit 1 review videos and projects
+  Mozilla Developer Network Website
+  Javascript & JQuery by Don Duckett
+  Elequent Javascript 2nd Edition by Marijn Haverbeke
+  Additional help via Treehouse Unit 1 Slack channel
+*/
 
 // Create the array of quote objects and name it quotes
 let quotes = [
-  {quote: 'quote 1.', source: 'billy', citation: 'c1', year: '1900'},
-  {quote: 'quote 2.', source: 'gary', citation: 'c2', year: '520'},
-  {quote: 'quote 3.', source: 'ralph', citation: 'c3', year: '1776'},
-  {quote: 'quote 4.', source: 'john', citation: 'c4', year: '1984'},
-  {quote: 'quote 5.', source: 'ron', citation: 'c5', year: '2001'},
+  {quote: "If you can dodge a wrench you can dodge a ball.", source: "Dodgeball", citation: "Patches O'Houlihan", year: "2004"},
+  {quote: "I'll be honest with you, I love his music. I really do. I'm a Michael Bolton fan.", source: "Office Space", citation: "Bob Slydell", year: "1999"},
+  {quote: "Just remember, when you control the mail you control...information", source: "Seinfeld"},
+  {quote: "I've lost the bleeps, I lost the sweeps and I lost the creeps.", source: "Spaceballs", year: "1987"},
+  {quote: "Dozens of people spontaneously combust each year. It's just not really widely reported.", source: "Spinal Tap", citation: "David St. Hubbins", year: "1984"},
 ];
 
 // Create the getRandomQuuote function and name it getRandomQuote
+//The function should take in one paramerter called array
 function getRandomQuote(array) {
+  //Select and return a random quote object from the quotes array
   let randomQuote = Math.floor(Math.random() * array.length);
+  //Created a variable to store information from the array
   let source = array[randomQuote];
+  //Returns information from the array of objects
   return source;
 }
 
 // Create the printQuote funtion and name it printQuote
 function printQuote() {
+  //Used to call on the array of quote objects and stored in variable
   let fetchQ = getRandomQuote(quotes);
+  //Create a new empty string and store it in a variable
   let placeText = " ";
+  //Used an if/else statement to handle various quote properties that may or may not exist. Concatenated strings using provided HTML template
   if (fetchQ.citation === undefined || fetchQ.year === undefined) {
     placeText += '<p class="quote">' + fetchQ.quote + '</p>' + '<p class="source">' + fetchQ.source;
   } else {
     placeText += '<p class="quote">' + fetchQ.quote + '</p>' + '<p class="source">' + fetchQ.source + '<span class="citation">' + fetchQ.citation + '</span>' + '<span class="year">' + fetchQ.year + '</span>' + '</p>';
   }
-
-/* Below is an alternative route but I still couldn't get it working.
-  let placeText = '<p class="quote"> ' + fetchQ.quote + ' </p>';
-  placeText += '<p class="source"> ' + fetchQ.source;
-
-  if (fetchQ.hasOwnProperty("citation")) {
-    placeText += '<span class="citation"> ' + fetchQ.citation + ' </span>';
-  }
-  if (fetchQ.hasOwnProperty("year")) {
-    placeText += '<span class="year"> ' + fetchQ.year + ' </span>';
-  }
-  placeText += '</p>';*/
-
+  //printQuote function should display the completed HTML string to the page using the below method
   document.getElementById('quote-box').innerHTML = placeText;
-
+}
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
